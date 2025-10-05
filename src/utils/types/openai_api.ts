54059ad7869f55b0504fe626232aac298aad71c3
ARLBR10 <71902040+ARLBR10@users.noreply.github.com>
2025-10-05 17:18:05 -0300

    feat(openai): Added messages to another file

--- src/utils/types/openai_api.ts
+++ src/utils/types/openai_api.ts
@@ -1,137 +1,26 @@
 import * as z from "zod"; // This thing is a fucking heavy
+import {
+  OpenAI_Message_Assistant,
+  OpenAI_Message_DeveloperSystem,
+  OpenAI_Message_Function,
+  OpenAI_Message_Tool,
+  OpenAI_Message_User,
+} from "./openai/messages";
 
 export const ChatCompletions_RequestBody = z.object({
   messages: z.array(
     z.union([
       // developer, system
-      z.object({
-        role: z.union([z.literal("developer"), z.literal("system")]),
-        content: z.array(
-          z.union([
-            z.string(),
-            z.object({
-              type: z.string(),
-              text: z.string(),
-            }),
-          ])
-        ),
-        name: z.optional(z.string()),
-      }),
+      OpenAI_Message_DeveloperSystem,
       // user
-      z.object({
-        role: z.literal("user"),
-        content: z.union([
-          z.string(),
-          z.array(
-            z.union([
-              z.object({
-                type: z.string(),
-                text: z.string(),
-              }),
-              z.object({
-                type: z.string(),
-                image_url: z.object({
-                  url: z.string(), // URL of the image or a BASE64 encoded
-                  detail: z.optional(z.string()),
-                }),
-              }),
-              z.object({
-                type: z.literal("input_audio"),
-                input_audio: z.object({
-                  data: z.string(),
-                  format: z.union([z.literal("wav"), z.literal("mp3")]),
-                }),
-              }),
-              z.object({
-                type: z.literal("file"),
-                file: z.object({
-                  file_data: z.optional(z.string()),
-                  file_id: z.optional(z.string()),
-                  filename: z.optional(z.string()),
-                  // OpenAI API reference lets everything as optional, so i guess its okay????
-                }),
-              }),
-            ])
-          ),
-        ]),
-        name: z.optional(z.string()),
-      }),
+      OpenAI_Message_User,
       // assistant
-      z.object({
-        role: z.literal("assistant"),
-        audio: z.optional(
-          z.object({
-            id: z.string(),
-          })
-        ),
-        content: z.union([
-          z.string(),
-          z.array(
-            z.union([
-              z.object({
-                text: z.string(),
-                type: z.string(),
-              }),
-              z.object({
-                refusal: z.string(),
-                type: z.string(),
-              }),
-            ])
-          ),
-        ]),
-        /**
-         * @deprecated use `tool_calls` instead
-         */
-        function_call: z.optional(
-          z.object({
-            arguments: z.string(),
-            name: z.string(),
-          })
-        ),
-        name: z.optional(z.string()),
-        refusal: z.optional(z.string()),
-        tool_calls: z.array(
-          z.union([
-            z.object({
-              function: z.object({
-                arguments: z.string(),
-                name: z.string(),
-              }),
-              id: z.string(),
-              type: z.string(),
-            }),
-            z.object({
-              custom: z.object({
-                input: z.string(),
-                name: z.string(),
-              }),
-              id: z.string(),
-              type: z.literal("custom"),
-            }),
-          ])
-        ),
-      }),
+      OpenAI_Message_Assistant,
       // tool
-      z.object({
-        content: z.union([
-          z.string(),
-          z.array(
-            z.object({
-              text: z.string(),
-              type: z.string(),
-            })
-          ),
-        ]),
-        role: z.literal("tool"),
-        tool_call_id: z.string(),
-      }),
+      OpenAI_Message_Tool,
       // @deprecated
       // function
-      z.object({
-        role: z.literal("function"),
-        name: z.string(),
-        content: z.string(),
-      }),
+      OpenAI_Message_Function,
     ])
   ),
   model: z.string(),
