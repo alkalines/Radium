c1ad5f0e5c2c95f0579c8f35b832d46e5d06db3a
ARLBR10 <71902040+ARLBR10@users.noreply.github.com>
2025-10-09 21:18:50 -0300

    feat(route): OpenAI Chat Completion with stream

--- src/app/api/openai/v1/chat/completions/route.ts
+++ src/app/api/openai/v1/chat/completions/route.ts
@@ -10,11 +10,34 @@ export async function POST(rawRequest: NextRequest) {
     // TODO: Cost tracking, and BYOK.
     const providerConnector = await AIBalancer(request);
     if (request.stream) {
+      const gen = await providerConnector.StreamCompletion(request);
+      const encoder = new TextEncoder();
 
+      const customReadable = new ReadableStream({
+        async start(controller) {
+          for await (const chunk of gen) {
+            controller.enqueue(encoder.encode(`data: ${JSON.stringify(chunk)}\n\n`));
+          }
+
+          // End of the stream
+          controller.enqueue(encoder.encode("data: [DONE]"));
+          controller.close()
+        },
+      });
+
+      // Server Sent Events (SSE)
+      return new Response(customReadable, {
+        headers: {
+          Connection: "keep-alive",
+          "Content-Encoding": "none",
+          "Cache-Control": "no-cache, no-transform",
+          "Content-Type": "text/event-stream; charset=utf-8",
+        },
+      });
     } else {
       const gen = await providerConnector.GenerateCompletion(request);
 
-      return NextResponse.json(gen)
+      return NextResponse.json(gen);
     }
   } catch (e) {
     if (e instanceof z.ZodError) {
