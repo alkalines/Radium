42538e8c4cfde83ba3dbe21f39322da6b8c97f2c
ARLBR10 <71902040+ARLBR10@users.noreply.github.com>
2025-10-17 23:26:04 -0300

    refactor(ai_balancer): Include provider info

--- src/utils/ai_balancer.ts
+++ src/utils/ai_balancer.ts
@@ -5,7 +5,13 @@ export default async function AIBalancer(Request: ChatCompletions_RequestBody_Ty
   /**
    * TODO: Load Balancer, and Credential manager, for the time being is just openrouter so fuck it.
    */
-  return new Providers.OpenRouter.connector({
-    apiKey: process.env.Openrouter_API_Key!
-  })
+  /**
+   * @temporary
+   */
+  return {
+    info: Providers.OpenRouter,
+    connector: new Providers.OpenRouter.connector({
+      apiKey: process.env.Openrouter_API_Key!
+    })
+  }
 }
